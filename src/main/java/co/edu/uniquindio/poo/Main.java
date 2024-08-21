5ea3eaf92bdec432666f80949d7b1eb37fb0c1fe
CesarBaquiro <cmbm.kf@gmail.com>
2024-08-21 09:52:41 -0500

    Add readme file

--- src/main/java/co/edu/uniquindio/poo/Main.java
+++ src/main/java/co/edu/uniquindio/poo/Main.java
@@ -2,6 +2,6 @@ package co.edu.uniquindio.poo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hola mundo!");
     }
 }
